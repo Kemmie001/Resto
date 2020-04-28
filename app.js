09e167fa1c84b7a1c3c97d169823a121c6f5dbe5
Kemmie001 <oluwakemisola757@gmail.com>
2020-04-28 11:19:34 +0100

    updated again

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-// next/previous controls
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-// images control
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-function showSlides(n){
+
+var slideIndex = 0;
+showSlides();
+function showSlides(){
     var i;
-    var slides = document.getElementsByClassName("slide");
-    var dot = document.getElementsByClassName("dot");
-    if (n > slides.length){
-        slideIndex = 1
-    }
-    if (n < 1){
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++)
-    {
-        slides[i].style.display = "none";
-    }
-    for (i=0; i < dot.length; i++){
-        dot[i].className = dot[i].className.replace("active", "");
-    }
-    slides[slideIndex-1].style.display ="block";
-    dot[slideIndex-1].className += "active";
-}
+     var slides = document.getElementsByClassName('slide');
+     for (i = 0; i < slides.length; i++){
+         slides[i].style.display = 'none';
+     }
+     slideIndex++;
+     if (slideIndex > slides.length)
+         {slideIndex = 1}
+     slides[slideIndex - 1].style.display = 'block';
+     setTimeout(showSlides, 1000);
+     }
+
