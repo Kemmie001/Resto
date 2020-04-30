cae8c403ee5fa2ed172b47ceaf5488ce31751a9a
Kemmie001 <oluwakemisola757@gmail.com>
2020-04-30 01:20:39 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 function showSlides(){
-    var i;
-     var slides = document.getElementsByClassName('slide');
+    let i;
+     let slides = document.getElementsByClassName('slide');
      for (i = 0; i < slides.length; i++){
          slides[i].style.display = 'none';
      }
@@ -11,6 +11,6 @@ function showSlides(){
      if (slideIndex > slides.length)
          {slideIndex = 1}
      slides[slideIndex - 1].style.display = 'block';
-     setTimeout(showSlides, 1000);
+     setTimeout(showSlides, 2000);
      }
 
