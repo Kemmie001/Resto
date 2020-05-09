c23ada7e8bd2dd846226d929abd85abfb91a077a
Kemmie001 <oluwakemisola757@gmail.com>
2020-05-09 18:24:56 +0100

    updated

--- app.js
+++ app.js
@@ -14,3 +14,30 @@ function showSlides(){
      setTimeout(showSlides, 2000);
      }
 
+var image = 1;
+showImage(image)
+
+function plusSlides(n){
+    showImage(image += n);
+}
+function currentSlide(n){
+    showImage(image = n);
+}
+function showImage(n){
+    var i; 
+    var images = document.getElementsByClassName('slide');
+    var dots = document.getElementsByClassName('dot');
+    if (n > images.length){
+        image = 1
+    }
+    if (n < 1 ){image = images.length}
+    for (i = 0; i < images.length; i++){
+        images[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className =
+        dots[i].className.replace(" active", "");
+    }
+    images[image - 1].style.display = "block";
+    dots[image - 1].className += 
+"active"}
