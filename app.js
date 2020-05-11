5087e767527f60efa4f69b48af3e93668f9c931b
Kemmie001 <oluwakemisola757@gmail.com>
2020-05-11 08:14:43 +0100

    updated

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
+var slideIndex = 1;
 
-let slideIndex = 0;
-showSlides();
-function showSlides(){
-    let i;
-     let slides = document.getElementsByClassName('slide');
-     for (i = 0; i < slides.length; i++){
-         slides[i].style.display = 'none';
-     }
-     slideIndex++;
-     if (slideIndex > slides.length)
-         {slideIndex = 1}
-     slides[slideIndex - 1].style.display = 'block';
-     setTimeout(showSlides, 2000);
-     }
+var myTimer;
 
-var image = 1;
-showImage(image)
-
+window.addEventListener("load", function(){
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){
+        plusSlides(1)
+    }, 2000);
+})
 function plusSlides(n){
-    showImage(image += n);
+    clearInterval(myTimer);
+    if(n < 0){
+        showSlides(slideIndex -= 1);
+    }else{
+        showSlides(slideIndex += 1);
+}
+if (n === -1){
+    myTimer = setInterval(function() {
+        plusSlides(n + 2)
+    }, 2000);
+}else{
+    myTimer = setInterval(function() {
+        plusSlides(n + 1)
+    }, 2000);
+}
 }
 function currentSlide(n){
-    showImage(image = n);
+    clearInterval(myTimer);
+    myTimer = setInterval(function() {plusSlides(n + 1)}, 2000);
+    showSlides(slideIndex = n);
 }
-function showImage(n){
-    var i; 
-    var images = document.getElementsByClassName('slide');
-    var dots = document.getElementsByClassName('dot');
-    if (n > images.length){
-        image = 1
+function showSlides (n){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length){
+        slideIndex = 1
     }
-    if (n < 1 ){image = images.length}
-    for (i = 0; i < images.length; i++){
-        images[i].style.display = "none";
+    if (n < 1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++){
-        dots[i].className =
         dots[i].className.replace(" active", "");
     }
-    images[image - 1].style.display = "block";
-    dots[image - 1].className += 
-"active"}
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
